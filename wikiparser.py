3e9a66aef680f46ccc20fd0160f2eaefadcabaf6
malaikannan <malai128@gmail.com>
2019-08-29 16:17:39 -0700

    added bloom filter and modified wikiparser for class

--- wikiparser.py
+++ wikiparser.py
@@ -2,62 +2,84 @@ import re
 import csv
 import os
 import argparse
+from bloomfilter import BloomFilter 
 
-def is_tamil(word):
-    first_char = word[:1]
-    lang_number = ord(first_char)
-    if lang_number >=int(lower_unicode_value) and lang_number <=int(upper_uniocde_value):
-        return True
+class wikiparser:
 
-def write_dict_csv(dict_tamil_word,file_name):
-    with open(file_name,"w") as f:
-        for word,count in dict_tamil_word.items():
-            write_line = word + "," + str(count) + os.linesep
-            f.write(write_line)
+    def __init__(self,wiki_dump_path,csv_file_path,bloomfilter_file_path,lower_unicode_value,upper_unicode_value):
+        self.wiki_dump_path = wiki_dump_path
+        self.csv_file_path = csv_file_path
+        self.bloomfilter_file_path = bloomfilter_file_path
+        self.lower_unicode_value = lower_unicode_value
+        self.upper_unicode_value = upper_unicode_value
+        print(self.lower_unicode_value)
+        print(self.upper_unicode_value)
+
+    @classmethod
+    def is_tamil(self,word,lower_unicode_value,upper_unicode_value):
+        first_char = word[:1]
+        lang_number = ord(first_char)
+        if lang_number >=int(lower_unicode_value) and lang_number <=int(upper_unicode_value):
+            return True
+
+    def create_csv_bloomfilter_files(self):
+        items_count = len(self.dict_tamil_word) 
+        falsepositive_probability = 0.001
+        bloomf = BloomFilter(items_count,falsepositive_probability) 
+
+        with open(self.csv_file_path,"w") as f:
+            for word,count in self.dict_tamil_word.items():
+                write_line = word + "," + str(count) + os.linesep
+                bloomf.add(word)
+                f.write(write_line)
+
+        bloomf.writetofile(self.bloomfilter_file_path)
 
 
-def parse_folders(folder_path,csv_file_path):
-    print(folder_path)
-    dict_tamil_word = {}
-    print(lower_unicode_value)
-    print(upper_uniocde_value)
 
-    for root, dirs, files in os.walk(folder_path):
-        print('Found directory: %s' % root)
-        for file in files:
-            if "wiki" in file:
-                print("processing file",file)
-                with open(os.path.join(root,file),'r') as xml_file:
-                    for line in xml_file.readlines():
-                        line = re.sub("[!@#$%&*.,<>:;|]", '', line)
-                        line = line.replace('"'," ")
-                        split_line = line.split(" ")
-                        for word in split_line:
-                            try:
-                                if(is_tamil(word)):
-                                    if word not in dict_tamil_word:
-                                        dict_tamil_word[word.strip()] = 1
-                                    else:
-                                        dict_tamil_word[word.strip()] = dict_tamil_word[word.strip()] + 1  
-                            except:
-                                pass
-    print("Number of unique words is",len(dict_tamil_word))
-    write_dict_csv(dict_tamil_word,csv_file_path)
+    def parse_folders(self):
+
+        self.dict_tamil_word = {}
+
+        for root, dirs, files in os.walk(self.wiki_dump_path):
+            print('Found directory: %s' % root)
+            for file in files:
+                if "wiki" in file:
+                    print("processing file",file)
+                    with open(os.path.join(root,file),'r') as xml_file:
+                        for line in xml_file.readlines():
+                            line = re.sub("[!@#$%&*.,<>:;|]", '', line)
+                            line = line.replace('"'," ")
+                            split_line = line.split(" ")
+                            for word in split_line:
+                                try:
+                                    if(self.is_tamil(word,self.lower_unicode_value,self.upper_unicode_value)):
+                                        if word not in self.dict_tamil_word :
+                                            self.dict_tamil_word [word.strip()] = 1
+                                        else:
+                                            self.dict_tamil_word [word.strip()] = self.dict_tamil_word [word.strip()] + 1  
+                                except:
+                                    pass
+        print("Number of unique words is",len(self.dict_tamil_word ))
+
+
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--wiki_dump_path')
 parser.add_argument('--csv_file_path')
+parser.add_argument("--bloomfilter_file_path")
 parser.add_argument("--lower_unicode_value")
 parser.add_argument("--upper_unicode_value")
 results = parser.parse_args()
-global lower_unicode_value, upper_uniocde_value 
-lower_unicode_value = results.lower_unicode_value
-upper_uniocde_value = results.upper_unicode_value
-parse_folders(results.wiki_dump_path,results.csv_file_path)
+
+parser = wikiparser(results.wiki_dump_path,results.csv_file_path,results.bloomfilter_file_path,results.lower_unicode_value,results.upper_unicode_value)
+parser.parse_folders() 
+parser.create_csv_bloomfilter_files()
+
 
 #Sample execution statement
-# python3 wikiparser.py --wiki_dump_path "/Users/malaikannan/Documents/Work/opensource/TamilData" --csv_file_path "/Users/malaikannan/Documents/Work/opensource/TamilData/tamil_words.csv" --lower_unicode_value 2944 --upper_unicode_value 3071 
+# python3 wikiparser.py --wiki_dump_path "/home/ANANT/msankarasubbu/Documents/Work/opensource/Data" --csv_file_path "/home/ANANT/msankarasubbu/Documents/Work/opensource/Data/tamil_words.csv"  --bloomfilter_file_path "/home/ANANT/msankarasubbu/Documents/Work/opensource/Data/tamil_words_filter.txt" --lower_unicode_value 2944 --upper_unicode_value 3071 
 
 
        
