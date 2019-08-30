2ecac214d482978f03f44edfcfd3c44f8ef1db7e
malaikannan <malai128@gmail.com>
2019-08-30 15:46:18 -0700

    added bengali and malayalam words too

--- bloomservice.py
+++ bloomservice.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 
 tamil_items_count = 1386991
 telugu_items_count = 1267283
+malayalam_items_count = 2181084
+bengali_items_count = 2230620
+
 falsepositive_probability = 0.001
 
-bloom_tamil = BloomFilter(tamil_items_count,falsepositive_probability,"/home/ANANT/msankarasubbu/Documents/Work/opensource/Data/TamilData/tamil_words_filter.txt")
-bloom_telugu = BloomFilter(telugu_items_count,falsepositive_probability,"/home/ANANT/msankarasubbu/Documents/Work/opensource/Data/TeluguData/telugu_words_filter.txt")
+bloom_tamil = BloomFilter(tamil_items_count,falsepositive_probability,"/home/malaikannan_sankarasubbu/Work/Data/tamil_words_filter.txt")
+bloom_telugu = BloomFilter(telugu_items_count,falsepositive_probability,"/home/malaikannan_sankarasubbu/Work/Data/telugu_words_filter.txt")
+bloom_malayalam = BloomFilter(malayalam_items_count,falsepositive_probability,"/home/malaikannan_sankarasubbu/Work/Data/malayalam_words_filter.txt")
+bloom_bengali = BloomFilter(bengali_items_count,falsepositive_probability,"/home/malaikannan_sankarasubbu/Work/Data/bengali_words_filter.txt")
+
 
 @app.route('/indicnlp/tamil/v1.0/<word>', methods=['GET'])
 def check_tamil_word(word):
@@ -29,6 +35,21 @@ def check_telugu_word(word):
     return jsonify({'Flag': Flag})
 
 
+@app.route('/indicnlp/malayalam/v1.0/<word>', methods=['GET'])
+def check_malayalam_word(word):
+    if bloom_malayalam.check(word):
+        Flag = True
+    else:
+        Flag = False
+    return jsonify({'Flag': Flag})
+
+@app.route('/indicnlp/bengali/v1.0/<word>', methods=['GET'])
+def check_bengali_word(word):
+    if bloom_bengali.check(word):
+        Flag = True
+    else:
+        Flag = False
+    return jsonify({'Flag': Flag})
 
 if __name__ == '__main__':
     app.run()
